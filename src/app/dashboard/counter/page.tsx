77bbfa74f79ddd7b3ed1dc9bd38b8915d7415c83
Joselu <joseluissgz97@gmail.com>
2024-09-22 10:12:19 +0200

    add: CounterPage

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,19 @@
+import { CartCounter } from '@/app/components/shopping-card/components/CartCounter';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Counter Page',
+  description: 'Local state example',
+};
+
+const CounterPage = () => {
+  return (
+    <section className="text-zinc-900 ">
+      <h2 className="mb-16 text-4xl font-bold p-4 ">Counter Page</h2>
+
+      <CartCounter />
+    </section>
+  );
+};
+
+export default CounterPage;

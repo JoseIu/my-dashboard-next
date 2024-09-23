4d369d91e2026c4b0e97adc0dc41b915d4923810
Joselu <joseluissgz97@gmail.com>
2024-09-23 20:09:25 +0200

    add: PokemonPageName and not-found page

---
+++ src/app/dashboard/pokemos/[name]/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const PokemonNotFoundPage = () => {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">Pokemon Not Found</div>
+      <button className="mt-5">
+        <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+            <Link href="/dashboard/pokemos">Go to Pokemons</Link>
+          </span>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default PokemonNotFoundPage;

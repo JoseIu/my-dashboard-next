6ecb6bc71dd8d0ceb3c6bdb09718fa62fa95343e
Joselu <joseluissgz97@gmail.com>
2024-09-23 13:27:28 +0200

    add: PokemonResponse interface

---
+++ src/interfaces/Pokemos.interface.ts
@@ -0,0 +1,16 @@
+export interface PokemonResponse {
+  count: number;
+  next: string;
+  previous: string;
+  results: Pokemon[];
+}
+
+export interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export interface SimplePokemon {
+  id: string;
+  name: string;
+}

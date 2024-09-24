deaba3d8059665c8300d187788381b10d3841324
Joselu <joseluissgz97@gmail.com>
2024-09-24 17:20:53 +0200

    add: PokemonsFavGrind on FavoritePokemonPage

--- src/app/dashboard/pokemos/favorite/page.tsx
+++ src/app/dashboard/pokemos/favorite/page.tsx
@@ -1,11 +1,11 @@
+import { PokemonFavGrid } from '@/components';
+
 const FavoritePokemonPage = () => {
   return (
     <section className="max-h-full max-w-full text-slate-900 p-2">
       <h2 className="text-4xl font-bold">Favorite Pokemos-List</h2>
 
-      <div className="max-w-full  h-[calc(100vh-4rem)] overflow-x-auto grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-6">
-        {/* <PokemonCard key={pokemon.id} pokemon={pokemon} /> */}
-      </div>
+      <PokemonFavGrid />
     </section>
   );
 };

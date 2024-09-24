a91b643e69892e99aca82d24680c88a682a708b9
Joselu <joseluissgz97@gmail.com>
2024-09-24 17:22:31 +0200

    feat: Add functionality to set favorite pokemons from local storage
    This commit adds a useEffect hook in the Providers component to retrieve the favorite pokemons from local storage and dispatches the setFavoritePokemons action to update the store with the retrieved data. This ensures that the favorite pokemons are persisted across page refreshes.

--- src/store/Providers.tsx
+++ src/store/Providers.tsx
@@ -1,11 +1,17 @@
 'use client';
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './';
+import { setFavoritePokemons } from './pokemons/pokemons';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Providers = ({ children }: Props) => {
+  useEffect(() => {
+    const favoritesPokemons = JSON.parse(localStorage.getItem('favorite-pokemons') || '{}');
+    store.dispatch(setFavoritePokemons(favoritesPokemons));
+  }, []);
   return <Provider store={store}>{children}</Provider>;
 };

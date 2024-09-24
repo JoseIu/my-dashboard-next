6a59c9bd199ae29b4c96f117642ba0bd3763edae
Joselu <joseluissgz97@gmail.com>
2024-09-24 17:20:34 +0200

    fix: fix max-height

--- src/app/dashboard/pokemos/page.tsx
+++ src/app/dashboard/pokemos/page.tsx
@@ -12,7 +12,7 @@ const PokemosPage = async () => {
     <section className="max-h-full max-w-full text-slate-900 p-2">
       <h2 className="text-4xl font-bold">Pokemos List</h2>
 
-      <div className="max-w-full  h-[calc(100vh-4rem)] overflow-x-auto grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-6">
+      <div className="max-w-full  max-h-[calc(100vh-4rem)] overflow-x-auto grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-6">
         {pokemos.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}

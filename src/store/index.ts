34c49419f2c0a1bff2316d4ea1380f9c6b01e207
Joselu <joseluissgz97@gmail.com>
2024-09-24 17:23:05 +0200

    add: pokemonsSlice and middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Middleware } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import counterSlice from './counter/counterSlice';
+import { localstorageMiddleware } from './middleware/localstorageMiddleware';
+import pokemonsSlice from './pokemons/pokemons';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    pokemons: pokemonsSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localstorageMiddleware as Middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
